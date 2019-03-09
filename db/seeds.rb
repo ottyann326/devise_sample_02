516a2317e9c11d1afcbd214adab47b01504c7f00
ottyann326 <ottyann326@gmail.com>
2019-03-09 17:43:38 +0900

    add index_new_create file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Item.create(name: "バルサ", price: 5000, user_id: 1)
+Item.create(name: "ブラックバルサ", price: 6000, user_id: 1)
+Item.create(name: "SK-7", price: 5000, user_id: 1)
